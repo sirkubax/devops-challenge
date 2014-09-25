f433478a5077050dc8e12d6b48edac9bdd817975
root <root@gui>
2014-09-25 11:59:04 +0200

    cleaning repo

--- start_env.sh
+++ start_env.sh
@@ -2,6 +2,6 @@
 
 source .env/bin/activate
 ./allegro-devop.py > /dev/null 2>&1 &
-redis-2.8.17/src/redis-server > /dev/null 2>&1 &
+redis-server > /dev/null 2>&1 &
 
 exit 0
--- stop_env.sh
+++ stop_env.sh
@@ -3,5 +3,6 @@
 killall redis-server
 ALLEGROPY=`ps aux |grep allegro-devop.py |grep -v grep|awk -F" " '{print $2}'`
 kill -9 $ALLEGROPY
+rm dump.rdb
 
 exit 0
